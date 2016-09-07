63122d7e065966eecb24febb85b21250cebbf6fc
SanaNasar <sanasehereen.cse@gmail.com>
2016-09-06 22:04:01 -0700

    adding prime factor js code

---
+++ prime_Factor.js
@@ -0,0 +1,21 @@
+// Run a while loop. start dividing by two and if not divisible increase divider until u r done.
+
+function primeFactors(n){
+  var factors = [],
+      divisor = 2;
+  
+  while(n>2){
+    if(n % divisor === 0){
+       factors.push(divisor);
+       n= n/ divisor;
+    }
+    else{
+      divisor++;
+    }
+  }
+  return factors;
+}
+
+primeFactors(69);
+
+// 3, 23]
