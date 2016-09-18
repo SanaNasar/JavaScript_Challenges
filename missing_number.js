7ad0e1f44c7524220f4bae612a0c4f893f078dc6
SanaNasar <sanasehereen.cse@gmail.com>
2016-09-18 14:10:48 -0700

    Adding missing number in an array

---
+++ missing_number.js
@@ -0,0 +1,16 @@
+// You have an array of numbers 1 to 100 in an array. 
+// Only one number is missing in the array. The array is unsorted. Find the missing number.
+function missingNumber(arr){
+  var n = arr.length+1,
+  sum = 0,
+  expectedSum = n* (n+1)/2;
+  
+  for(var i = 0, len = arr.length; i < len; i++){
+    sum += arr[i];
+  }
+  
+  return expectedSum - sum;
+}
+
+missingNumber([5, 2, 6, 1, 3]);
+  // = 4
