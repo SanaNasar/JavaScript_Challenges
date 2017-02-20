d263e82a657f545071fe94dbce8ce73f0f1e8d49
SanaNasar <sanasehereen.cse@gmail.com>
2017-02-20 10:33:38 -0800

    adding javascript abstractions

---
+++ abstractions.js
@@ -0,0 +1,21 @@
+// Javascript abstractions demo
+
+var work = function() {
+  console.log("working hard!");
+};
+
+var dowork = function(f){
+
+  console.log("starting");
+  // error handling
+  try {
+      f();
+  }
+  catch(ex) {
+    console.log(ex);
+  }
+  console.log("end");
+};
+
+// function calling - note: not invoking but passing
+dowork(work);
