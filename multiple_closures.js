ca1a9336f99eca2641b425a7b54e028588a1b8f8
SanaNasar <sanasehereen.cse@gmail.com>
2017-08-07 11:08:38 -0700

    adding multiple closures examples

---
+++ multiple_closures.js
@@ -0,0 +1,17 @@
+// Simple JavaScript function:
+
+function sayHello(name) {
+  var text = 'Hello ' + name;
+  var say = function() {
+    console.log(text);
+  };
+  say();
+}
+sayHello('Joe');
+// Prints
+//Hello Joe
+
+// Examples of Closures
+// Example 1
+
+
