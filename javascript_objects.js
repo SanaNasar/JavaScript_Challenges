81db6cd71c1546d83f379a8ae945da64a13576e5
SanaNasar <sanasehereen.cse@gmail.com>
2017-11-14 20:49:23 -0800

    adding ref and primitive datatypes in js

--- javascript_objects.js
+++ javascript_objects.js
@@ -2,8 +2,18 @@
 var myFirstObject = {firstName: "Richard", favoriteAuthor: "Conrad"};
 
 var ageGroup = {30: "Children", 100:"Very Old"};
-console.log(ageGroup.30) // This will throw an error​
+console.log(ageGroup.30);
+// This will throw an error​
 ​// This is how you will access the value of the property 30, to get value "Children"​
 console.log(ageGroup["30"]); // Children​
-​
+
 ​//It is best to avoid using numbers as property names.
+
+// Reference and Primitive datatypes.
+// The primitive data type String is stored as a value​
+​var person = "Kobe";  
+​var anotherPerson = person; // anotherPerson = the value of person​
+person = "Bryant"; // value of person changed​
+​
+console.log(anotherPerson); // Kobe​
+console.log(person); // Bryant
