1ddb746ede71270af401bedc122dba7de8246759
SanaNasar <sanasehereen.cse@gmail.com>
2017-11-14 20:26:37 -0800

    adding js objects basics

---
+++ javascript_objects.js
@@ -0,0 +1,9 @@
+// example of a simple object
+var myFirstObject = {firstName: "Richard", favoriteAuthor: "Conrad"};
+
+var ageGroup = {30: "Children", 100:"Very Old"};
+console.log(ageGroup.30) // This will throw an error​
+​// This is how you will access the value of the property 30, to get value "Children"​
+console.log(ageGroup["30"]); // Children​
+​
+​//It is best to avoid using numbers as property names.
